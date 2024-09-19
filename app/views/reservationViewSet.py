3770cda2c2c360a7de6feac1b3ccb1e407f8dcab
이용주 <nyk700@joonggonara.co.kr>
2024-09-19 21:43:17 +0900

    feat: 권한 관련 코멘트 추가

--- app/views/reservationViewSet.py
+++ app/views/reservationViewSet.py
@@ -17,9 +17,9 @@ class ReservationViewSet(ModelViewSet):
 
     def get_permissions(self):
         if self.action == 'create':
-            permission_classes = [IsClientUser]
+            permission_classes = [IsClientUser]  # 예약 신청은 고객만 가능
         elif self.request.user.is_authenticated and not self.request.user.is_admin:
-            permission_classes = [IsOwner]
+            permission_classes = [IsOwner]  # 예약 상세 조회, 수정, 삭제 시 고객은 본인 것만 가능
         else:
             permission_classes = self.permission_classes
         return [permission() for permission in permission_classes]
@@ -27,5 +27,5 @@ class ReservationViewSet(ModelViewSet):
     def get_queryset(self):
         if self.action == 'list':
             if self.request.user.is_authenticated and not self.request.user.is_admin:
-                return Reservation.objects.filter(user=self.request.user)
+                return Reservation.objects.filter(user=self.request.user)    # 예약 리스트 조회 시 고객은 본인 것만 노출
         return Reservation.objects.all()
